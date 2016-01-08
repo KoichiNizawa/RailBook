20c4bdf45d582ac6d3ab8a775789db9785dc86a8
Koichi Nizawa <nizawa@jhsc.co.jp>
2016-01-08 13:16:54 +0900

    検索機能改良

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -63,6 +63,7 @@ class BooksController < ApplicationController
 
   def search
     @books = Book.where(title: params["search"]["title"])
+    @p_title = params["search"]["title"]
     render :index
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107042307) do
+ActiveRecord::Schema.define(version: 20160108005155) do
 
   create_table "books", force: true do |t|
     t.string   "isbn"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20160107042307) do
     t.string   "publish"
     t.date     "published"
     t.boolean  "cd"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "publishes", force: true do |t|
+    t.string   "publishname"
+    t.string   "address"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
