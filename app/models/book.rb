f966a1af180402f664c542e4307e13a07124d429
Koichi Nizawa <nizawa@jhsc.co.jp>
2016-01-08 13:49:35 +0900

    レビュー機能の追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
+	has_many :reviews
 	validates :isbn, presence: true, uniqueness: true
 	validates :title, length: {minimum: 1, muximum: 100}
 	validates :price, numericality: {only_integer: true}
